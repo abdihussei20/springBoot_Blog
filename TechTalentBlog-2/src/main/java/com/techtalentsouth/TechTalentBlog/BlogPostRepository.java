69ae9cb49d1a2c69bbeebf3d0989e6de66cbb92a
abdih <abdihussei2o@gmail.com>
2022-11-15 17:42:44 -0500

    "first commit"

---
+++ TechTalentBlog-2/src/main/java/com/techtalentsouth/TechTalentBlog/BlogPostRepository.java
@@ -0,0 +1,9 @@
+package com.techtalentsouth.TechTalentBlog;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface BlogPostRepository extends CrudRepository<BlogPost, Long>{
+	
+
+}
---
+++ TechTalentBlog-2/src/main/java/com/techtalentsouth/TechTalentBlog/TechTalentBlog2Application.java
@@ -0,0 +1,13 @@
+package com.techtalentsouth.TechTalentBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTalentBlog2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTalentBlog2Application.class, args);
+	}
+
+}
